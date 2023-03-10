43ac17969e1c270de1b0095b151d8a8a95c8e665
Conor Walton <waltoncon@users.noreply.github.com>
2023-03-10 20:03:07 +0000

    feat: add link to cew.sh

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="font-mono grid place-items-center h-screen">
-        <div class="flex flex-col items-center gap-4">
+        <div class="flex flex-col gap-4">
             <h1 class="text-3xl font-bold">
                 Conor Walton
             </h1>
@@ -9,6 +9,12 @@
             </Link>
             <Link href="https://github.com/waltoncon">
                 <div class="i-mdi:github" /> waltoncon
+            </Link>
+            <h2 class="text-2xl font-bold mt-3">
+                Projects
+            </h2>
+            <Link href="https://cew.sh">
+                <div class="i-mdi:link" /> Utilities
             </Link>
         </div>
     </div>
