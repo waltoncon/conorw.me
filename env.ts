620dbef207b7a8684ef77a7bcc2274971028bb6d
Conor Walton <7566870+waltoncon@users.noreply.github.com>
2025-02-04 12:20:33 +0000

    setup posthog rewrites

--- env.ts
+++ env.ts
@@ -5,10 +5,8 @@ import { z } from "zod";
 export const env = createEnv({
   client: {
     NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
-    NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
-    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   },
 });
